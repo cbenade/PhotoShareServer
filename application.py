f0ac5990062906511087891c8a509d0a84d74e6a
cbenade <christian.benade@live.ca>
2019-11-25 22:56:51 -0800

    Changed implimentation of broadcasting

--- application.py
+++ application.py
@@ -9,13 +9,19 @@ socketio = SocketIO(app)
 @socketio.on('connect')
 def user_has_connected():
     print('a user connected')
-    emit('event1')
+    userHasConnected()
 
 # Print message when client disconnects
 @socketio.on('disconnect')
 def user_has_disconnected():
     print('a user disconnected')
-    emit('event2')
+    userHasDisconnected()
+
+def userHasConnected():
+    emit('event1', broadcast=True)
+
+def userHasDisconnected():
+    emit('event2', broadcast=True)
 
 # Run gevent web server
 if __name__ == '__main__':
