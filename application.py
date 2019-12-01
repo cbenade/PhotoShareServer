9a34087a847be60ff0f92be68a1849bdaf67ff16
cbenade <christian.benade@live.ca>
2019-12-01 10:45:32 -0800

    Test changes to server design

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_socketio import SocketIO, emit, Namespace
+from flask_socketio import SocketIO, emit
 
 # Create flask and socketio objects
 app = Flask(__name__)
@@ -8,27 +8,19 @@ socketio = SocketIO(app)
 # Print message when client connects
 @socketio.on('connect')
 def user_has_connected():
-    print('a user connected')
-    userHasConnected()
-def userHasConnected():
-    emit('aUserHasConnected', broadcast=True)
+    print('a user connected to a group')
+    emit('aUserHasConnectedToAGroup', broadcast=True)
 
 # Print message when client disconnects
 @socketio.on('disconnect')
 def user_has_disconnected():
-    print('a user disconnected')
-    userHasDisconnected()
-def userHasDisconnected():
-    emit('aUserHasDisconnected', broadcast=True)
+    print('a user disconnected to a group')
+    emit('aUserHasDisconnectedToAGroup', broadcast=True)
 
-@socketio.on('clientMessage')
-def received_client_message(message):
-    print('received client message: ' + message)
-    # response = message + "123"
-    namespace = message
-    emit_server_message(namespace)
-def emit_server_message(namespace):
-    emit("serverMessage", namespace, namespace=namespace)
+@socketio.on('clientPhotoUploaded')
+def broadcast_image_uploaded():
+    print('received client message')
+    emit("imageUploadedToGroupName", broadcast=True)
 
 # Run gevent web server
 if __name__ == '__main__':
