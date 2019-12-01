375e3e8ce4f75ead4773d6590c685052780581eb
cbenade <christian.benade@live.ca>
2019-12-01 10:55:29 -0800

    Fixed server to properly send/recieve messages

--- application.py
+++ application.py
@@ -8,19 +8,25 @@ socketio = SocketIO(app)
 # Print message when client connects
 @socketio.on('connect')
 def user_has_connected():
-    print('a user connected to a group')
-    emit('aUserHasConnectedToAGroup', broadcast=True)
+    print('a user connected')
+    userHasConnected()
+def userHasConnected():
+    emit('aUserHasConnected', broadcast=True)
 
 # Print message when client disconnects
 @socketio.on('disconnect')
 def user_has_disconnected():
-    print('a user disconnected to a group')
-    emit('aUserHasDisconnectedToAGroup', broadcast=True)
+    print('a user disconnected')
+    userHasDisconnected()
+def userHasDisconnected():
+    emit('aUserHasDisconnected', broadcast=True)
 
-@socketio.on('clientPhotoUploaded')
+@socketio.on('uploadNotification')
+def notified_image_uploaded():
+    print('received client notification')
+    broadcast_image_uploaded()
 def broadcast_image_uploaded():
-    print('received client message')
-    emit("imageUploadedToGroupName", broadcast=True)
+    emit("imageUploaded", broadcast=True)
 
 # Run gevent web server
 if __name__ == '__main__':
