535e0aaa553bdb93240fadf39636839e768db162
cbenade <christian.benade@live.ca>
2019-12-02 22:12:35 -0800

    Test2

--- application.py
+++ application.py
@@ -9,31 +9,31 @@ socketio = SocketIO(app)
 @socketio.on('connect')
 def user_has_connected():
     print('a user connected')
-    emit('aUserHasConnected', broadcast=True)
+    # emit('aUserHasConnected', broadcast=True)
 
-#     userHasConnected()
-# def userHasConnected():
-#     emit('aUserHasConnected', broadcast=True)
+    userHasConnected()
+def userHasConnected():
+    emit('aUserHasConnected', broadcast=True)
 
 # Print message when client disconnects
 @socketio.on('disconnect')
 def user_has_disconnected():
     print('a user disconnected')
-    emit('aUserHasDisconnected', broadcast=True)
+    # emit('aUserHasDisconnected', broadcast=True)
 
-#     userHasDisconnected()
-# def userHasDisconnected():
-#     emit('aUserHasDisconnected', broadcast=True)
+    userHasDisconnected()
+def userHasDisconnected():
+    emit('aUserHasDisconnected', broadcast=True)
 
 # Broadcast image upload notification to connected devices
 @socketio.on('uploadNotification')
 def notified_image_uploaded():
     print('received client notification')
-    emit("imageUploaded", broadcast=True)
+    # emit("imageUploaded", broadcast=True)
 
-#     broadcast_image_uploaded()
-# def broadcast_image_uploaded():
-#     emit("imageUploaded", broadcast=True)
+    broadcast_image_uploaded()
+def broadcast_image_uploaded():
+    emit("imageUploaded", broadcast=True)
 
 # Run gevent web server
 if __name__ == '__main__':
